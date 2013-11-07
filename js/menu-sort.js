b3a4e9c4b026cd3d5696c1f51c336f09ea9834fc
unknown <iguest@is-m430m27-18.ischool.uw.edu>
2013-11-06 23:13:33 -0800

    JQuery...WHY YOU SO HARD?!

--- js/menu-sort.js
+++ js/menu-sort.js
@@ -32,31 +32,81 @@
 
 	//} //for each pizza
 //}
-function render(pizzas, meatTemplate, vegTemplate, pizzaMenu) {
+
+
+// function render(pizzas, meatTemplate, vegTemplate, pizzaMenu) {
+// 	var instance;
+// 	pizzaMenu.empty();
+// 	$.each(pizzas, function(){
+// 		if (this.vegetarian) {
+// 			instance = vegTemplate.clone();
+// 		} else {
+// 			instance = meatTemplate.clone();
+// 		}
+
+// 		instance.find('.name').html(this.name);
+// 		instance.find('.description').html(this.description);
+// 		instance.find('.prices').html("$" + this.prices[0] + "/$" + this.prices[1] + "/$" + this.prices[2]);
+
+// 		if (this.vegetarian) {
+// 			instance.removeClass('vegTemplate');
+// 		} else {
+// 			instance.removeClass('meatTemplate');
+// 		}
+
+// 		pizzaMenu.append(instance);
+// 	});
+// }
+
+function render(pizzas, meatTemplate, vegTemplate, container1, container2) {
 	var instance;
-	meatTemplate.empty();
-	vegTemplate.empty();
-	$.each(pizzas, function(){
-		if (pizzas.vegetarian) {
+	container1.empty();
+	container2.empty();
+	$.each(pizzas, function() {
+		if (this.vegetarian) {
 			instance = vegTemplate.clone();
 		} else {
 			instance = meatTemplate.clone();
 		}
 
 		instance.find('.name').html(this.name);
-			instance.find('.description').html(this.description);
-			instance.find('.prices').html("$" + this.prices[0] + "/$" + this.prices[1] + "/$" + this.prices[2]);
+		instance.find('.description').html(this.description);
+		instance.find('.prices').html("$" + this.prices[0] + "/$" + this.prices[1] + "/$" + this.prices[2]);
 
-		if (pizzas.vegetarian) {
+		if (this.vegetarian) {
 			instance.removeClass('vegTemplate');
+			container2.append(instance);
 		} else {
 			instance.removeClass('meatTemplate');
+			container1.append(instance);
 		}
-
-		pizzaMenu.append(instance);
 	});
 }
 
+// function render(pizzas, meatTemplate, vegTemplate, pizzaMenu) {
+// 	var idx;
+// 	var pizza;
+// 	var instance;
+// 	// pizzaMenu.empty();
+// 	for (idx = 0; idx < pizzas.length; ++idx) {
+// 		pizza = pizzas[idx];
+// 		if (pizza.vegetarian) {
+// 			instance = vegTemplate.clone();
+// 		} else {
+// 			instance = meatTemplate.clone();
+// 		}
+// 		$('.name').append(pizza.name);
+// 		$('.description').append(pizza.description);
+// 		$('.prices').append("$" + pizza.prices[0] + "/$" + pizza.prices[1] + "/$" + pizza.prices[2]);
+
+// 		if (this.vegetarian) {
+// 			instance.removeClass('vegTemplate');
+// 		} else {
+// 			instance.removeClass('meatTemplate');
+// 		}
+// 	}
+// }
+
 $(function(){
-	render(com.dawgpizza.menu.pizzas, $('.meatTemplate'), $('.vegTemplate'), $('.pizzaMenu'));
+	render(com.dawgpizza.menu.pizzas, $('.meatTemplate'), $('.vegTemplate'), $('.container1'), $('.container2'));
 }); //document ready()
